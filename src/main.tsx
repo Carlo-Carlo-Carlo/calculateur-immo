eb713e38cb224aa435c75e7f14c210cb31acd5b9
Carlo-Carlo-Carlo <carlo.mologni@gmail.com>
2026-01-19 23:08:56 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App'
+import App from './App.tsx'
+import { MentionsLegales, PolitiqueConfidentialite } from './LegalPages.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/mentions-legales" element={<MentionsLegales />} />
+        <Route path="/politique-confidentialite" element={<PolitiqueConfidentialite />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
