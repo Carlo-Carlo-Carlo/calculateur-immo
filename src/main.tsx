ed6a9eedce6f1df1e3afdf4cfdec67c5f229d04e
Carlo-Carlo-Carlo <carlo.mologni@gmail.com>
2026-01-26 17:17:58 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,26 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import { MentionsLegales, PolitiqueConfidentialite } from './LegalPages'
 import CapaciteEmprunt from './pages/CapaciteEmprunt'
 
+// Composant pour scroll en haut à chaque changement de page
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  
+  return null;
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/mentions-legales" element={<MentionsLegales />} />
