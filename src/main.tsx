3999bc98b850597dfc00726ccdb7ef01960d31d1
Carlo-Carlo-Carlo <carlo.mologni@gmail.com>
2026-01-27 10:26:47 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import App from './App'
 import { MentionsLegales, PolitiqueConfidentialite } from './LegalPages'
 import CapaciteEmprunt from './pages/CapaciteEmprunt'
 import TauxEndettement from './pages/TauxEndettement'
+import ResteAVivre from './pages/ResteAVivre'
 
 // Composant pour scroll en haut à chaque changement de page
 function ScrollToTop() {
@@ -28,6 +29,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/politique-confidentialite" element={<PolitiqueConfidentialite />} />
         <Route path="/calcul-capacite-emprunt" element={<CapaciteEmprunt />} />
         <Route path="/taux-endettement-35-pourcent" element={<TauxEndettement />} />
+        <Route path="/calcul-reste-a-vivre" element={<ResteAVivre />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
